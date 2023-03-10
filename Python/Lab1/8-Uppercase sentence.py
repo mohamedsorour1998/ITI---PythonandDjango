58fe609f8c906bfd9f28edec7bf78f3d821e8466
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2023-03-10 14:25:03 +0200

    Finished TASK8! Uppercase sentence

---
+++ Python/Lab1/8-Uppercase sentence.py
@@ -0,0 +1,9 @@
+string = input("Please tell me the string so i will uppercase  it: ")
+
+
+def uppercaseTheString(string):
+    string = string.upper()
+    return string
+
+
+print("the string in uppercase:", uppercaseTheString(string))
